5fb3b3f0adfaca1ab577084694dc05664ad2f9dd
Blaz Sajovic <blaz.sajovic@spletko.eu>
2013-08-20 21:37:13 +0200

    upd

--- app/controllers/domov_controller.rb
+++ app/controllers/domov_controller.rb
@@ -2,6 +2,6 @@ class DomovController < ApplicationController
   require 'youtube_it'
 
   def index
-  	@blogs = Blog.all.order("created_at DESC").limit(2)
+  	@blog = Blog.all.order("created_at DESC").limit(2)
   end
 end
